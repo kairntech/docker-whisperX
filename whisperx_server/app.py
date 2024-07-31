dc7b504a037784a2876ae4c09b5bcfa54a09bf73
Olivier Terrier <olivier.terrier@kairntech.com>
2024-07-31 13:30:37 +0200

    task="transcribe"

--- whisperx_server/app.py
+++ whisperx_server/app.py
@@ -124,7 +124,7 @@ def task(options: InputOptions):
                 language = options.language if options.language != 'xx' else None
             else:
                 language = options.language
-            result = app.model.transcribe(audio, language=language, batch_size=options.batch_size)
+            result = app.model.transcribe(audio, task="transcribe", language=language, batch_size=options.batch_size)
             detected_language = result["language"]
             elapsed_time = time.time_ns() / 1e6 - start_time
             logger.info(f"Duration to transcribe audio: {elapsed_time:.2f} ms")
